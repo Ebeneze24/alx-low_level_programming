77150e74e6963fb24a35ebf21e68ae76a60e6be1
Ebeneze24 <132400531+Ebeneze24@users.noreply.github.com>
2023-06-19 23:22:13 +0100

    Create 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Main.
+*
+* Return: int
+*/
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" -");
+	fprintf(stderr, " Dora Korpar, 2015-10-19\n");
+	return (EXIT_FAILURE);
+}
