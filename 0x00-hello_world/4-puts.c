8c645622b5afaf5dfb91ed931082297cfedfec3b
Ebeneze24 <132400531+Ebeneze24@users.noreply.github.com>
2023-06-19 23:12:18 +0100

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+	* main - execution starts here
+	* Return: int (0 == success, 1 == error)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
